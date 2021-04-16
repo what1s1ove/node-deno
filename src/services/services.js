dba16053e1da181d9fb96e55e2593bf4cf0ddd17
what1s1ove <zubko.2007@gmail.com>
2021-04-16 08:13:15 +0300

    master: + http service

--- src/services/services.js
+++ src/services/services.js
@@ -1,10 +1,13 @@
 const { Book } = require('./book/book.service');
+const { Http } = require('./http/http.service');
 
 const initServices = () => {
+  const http = new Http();
   const book = new Book();
 
   return {
     book,
+    http,
   };
 };
 
